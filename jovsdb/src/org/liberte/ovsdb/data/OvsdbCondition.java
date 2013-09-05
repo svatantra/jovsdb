5425884a983b586fa271a2d27f06837fffe51e8f
mritesh <ritesh.liberte@gmail.com>
2013-09-05 12:19:15 +0530

    Ovsdbvalue

--- jovsdb/src/org/liberte/ovsdb/data/OvsdbCondition.java
+++ jovsdb/src/org/liberte/ovsdb/data/OvsdbCondition.java
@@ -1,5 +1,22 @@
 package org.liberte.ovsdb.data;
 
+import org.liberte.ovsdb.data.OvsdbValue;
+
+enum ConditionFunction{
+	LESS_THAN,
+	LESS_THAN_EQUAL_TO,
+	EQUAL_TO,
+	GREATER_THAN,
+	GREATER_THAN_EQUAL_TO,
+	NOT_EQUAL_TO,
+	INCLUDES,
+	EXCLUDES	
+}
+
 public class OvsdbCondition {
+	
+	private String column;
+	private ConditionFunction  function;
+	private OvsdbValue value;
 
 }
