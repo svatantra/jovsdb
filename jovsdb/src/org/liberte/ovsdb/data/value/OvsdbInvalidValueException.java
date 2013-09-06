665add36e14812888e93ee6e0210d9e909972e30
svatantra <svatantra.liberte@gmail.com>
2013-09-06 10:20:12 +0530

    Exception

---
+++ jovsdb/src/org/liberte/ovsdb/data/value/OvsdbInvalidValueException.java
@@ -0,0 +1,12 @@
+package org.liberte.ovsdb.data.value;
+
+public class OvsdbInvalidValueException extends Exception {
+
+	public OvsdbInvalidValueException(String name) {
+		super(name);
+		
+	}
+
+	
+
+}
