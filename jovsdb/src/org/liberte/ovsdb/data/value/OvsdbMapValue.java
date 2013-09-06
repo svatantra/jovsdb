2c06b96452b46b47faf52eb415bc52ba02bda699
svatantra <svatantra.liberte@gmail.com>
2013-09-06 10:03:18 +0530

    Map Value

---
+++ jovsdb/src/org/liberte/ovsdb/data/value/OvsdbMapValue.java
@@ -0,0 +1,45 @@
+package org.liberte.ovsdb.data.value;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OvsdbMapValue extends OvsdbValue {
+
+	// members
+	private HashMap<OvsdbValue, OvsdbValue> value;
+
+	public OvsdbMapValue() {
+		this("");
+
+	}
+
+	public OvsdbMapValue(String name) {
+		super(name, OvsdbValueType.MAP);
+
+	}
+
+	public HashMap<OvsdbValue, OvsdbValue> getValue() {
+		return value;
+	}
+
+	public void setValue(HashMap<OvsdbValue, OvsdbValue> value) {
+		this.value = value;
+	}
+
+	public void addPair(OvsdbValue key, OvsdbValue val) {
+		this.value.put(key, val);
+	}
+
+	public String getValueAsString() {
+		StringBuilder s = new StringBuilder();
+		for (Map.Entry<OvsdbValue, OvsdbValue> entry : value.entrySet()) {
+			OvsdbValue key = entry.getKey();
+			OvsdbValue val = entry.getValue();
+			s.append(key);
+			s.append(":");
+			s.append(val);
+			s.append(",");
+		}
+		return s.toString();
+	}
+}
